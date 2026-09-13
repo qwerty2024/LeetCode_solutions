4000af590822a4ee1b7213f4df9534eb6cdc809f
qwerty2024 <multanmgu@gmail.com>
2026-09-13 22:51:05 +0300

    835

---
+++ mideum/835.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int n = 0;
+
+private:
+    int test(vector<vector<int>>& img1, vector<vector<int>>& img2, int i, int j) {
+        int res = 0;
+        for (int ii = 0; ii < n; ii++) {
+            for (int jj = 0; jj < n; jj++) {
+                if (ii + i >= 0 && jj + j >= 0 && ii + i < n && jj + j < n) {
+                    if (img1[ii + i][jj + j] == 1 && img2[ii][jj] == 1) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        n = img1.size();
+        int ans = 0;
+
+        for (int i = 1 - n; i < 2 * n; i++) {
+            for (int j = 1 - n; j < 2 * n; j++) {
+                ans = max(ans, test(img1, img2, i, j));
+            }
+        }
+
+        return ans;
+    }
+};
