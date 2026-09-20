1f0497230e7fed5f8c989bd56f5e55a3ac09a73b
qwerty2024 <multanmgu@gmail.com>
2026-09-20 23:54:02 +0300

    3498

---
+++ easy/3498.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        unordered_map<char, int> um = {{'a', 26}, {'b', 25}, {'c', 24}, {'d', 23}, {'e', 22}, {'f', 21}, {'g', 20}, {'h', 19}, {'i', 18}, {'j', 17}, {'k', 16}, {'l', 15}, {'m', 14}, {'n', 13}, {'o', 12}, {'p', 11}, {'q', 10}, {'r', 9}, {'s', 8}, {'t', 7}, {'u', 6}, {'v', 5}, {'w', 4}, {'x', 3}, {'y', 2}, {'z', 1}};
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            ans += (i + 1) * um[s[i]];
+        }
+
+        return ans;
+    }
+};
